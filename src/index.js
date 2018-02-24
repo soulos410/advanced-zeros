aba935ff73da43e25e1fc064bf283c52eabd1c60
Siarhei <solimr111@mail.ru>
2018-02-24 16:31:14 +0300

    completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let exponent, inputBase = base, numberOfZeros = number,counter, preAnswer;
+
+  for (let myBase = 2; myBase <= base; myBase++){
+    if (inputBase % myBase === 0){
+      exponent = 0;
+      while (inputBase % myBase === 0){
+        inputBase /= myBase;
+        exponent++;
+      } 
+      counter = 0;
+      numberCopy = number
+      while (numberCopy / myBase > 0){
+        counter += Math.floor(numberCopy / myBase);
+        numberCopy /= myBase ;
+      }
+      preAnswer = Math.floor(counter / exponent);
+      numberOfZeros = Math.min(numberOfZeros,preAnswer);
+  }
+}
+  return numberOfZeros;
 }
